a5ff9a05376ee97c4165ee544fa542d778b41d93
Raphael D. Pinheiro <raphaklaus@gmail.com>
2020-06-24 22:18:28 +0200

    wow

--- bin/deps.sh
+++ bin/deps.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+#!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+source .bashrc
+nvm install node
+nvm use node
+
 cd /var/www/html/website
 npm install
